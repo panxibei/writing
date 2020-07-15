4c92a43ba2216a880a0bcdeed9246102cba8677a
panxibei <panxibei@yandex.com>
2020-07-15 14:52:28 +0800

    writing

---
+++ 55_系统运维/202003191047_一招找回Win10的照片查看器/恢复Win10照片查看器.bat
@@ -0,0 +1,31 @@
+@echo off&cd\&color 0a&cls
+ 
+echo »Ö¸´Win10ÕÕÆ¬²é¿´Æ÷
+ 
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpg" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpeg" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".webp" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".pcx" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".bmp" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".tga" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".exif" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".svg" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".psd" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".cdr" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".pcd" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".fpx" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".ufo" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".eps" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".ai" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".gif" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".wmf" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".flic" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".hdri" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".raw" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".emf" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".ico" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
+ 
+echo ÇëË«»÷»òÓÒ»÷Í¼Æ¬£¬Ñ¡Ôñ¡°ÕÕÆ¬²é¿´Æ÷¡±¼´¿É
+ 
+pause
