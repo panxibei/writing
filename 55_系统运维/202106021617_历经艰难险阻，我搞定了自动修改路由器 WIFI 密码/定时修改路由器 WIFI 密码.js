f5214758e8975c10d2c18624a19d382a4d6d82c6
panxibei <panxibei@yandex.com>
2021-06-03 14:53:17 +0800

    writing

--- 55_系统运维/202106021617_历经艰难险阻，我搞定了自动修改路由器 WIFI 密码/定时修改路由器 WIFI 密码.js
+++ 55_系统运维/202106021617_历经艰难险阻，我搞定了自动修改路由器 WIFI 密码/定时修改路由器 WIFI 密码.js
@@ -16,88 +16,97 @@
 
     //页面完全加载后运行
 	window.onload=function autorun() {
+		
+		console.log('页面加载完毕，可以执行代码！！');
 
-        var currentDate = '';
-        var checkDate = '';
-
-		function changeWifi() {
-			Date.prototype.Format = function (fmt) {
-				let o = {
-					"M+": this.getMonth() + 1, //月份
-					"d+": this.getDate(), //日
-					"h+": this.getHours(), //小时
-					"m+": this.getMinutes(), //分
-					"s+": this.getSeconds(), //秒
-					"q+": Math.floor((this.getMonth() + 3) / 3), //季度
-					"S": this.getMilliseconds() //毫秒
-				};
-				if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-				for (let k in o) {
-					if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-				}
-				return fmt;
+		Date.prototype.Format = function (fmt) {
+			let o = {
+				"M+": this.getMonth() + 1, //月份
+				"d+": this.getDate(), //日
+				"h+": this.getHours(), //小时
+				"m+": this.getMinutes(), //分
+				"s+": this.getSeconds(), //秒
+				"q+": Math.floor((this.getMonth() + 3) / 3), //季度
+				"S": this.getMilliseconds() //毫秒
 			};
+			if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+			for (let k in o) {
+				if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+			}
+			return fmt;
+		};
+		
+        var currentDate = (new Date()).Format("yyyyMMdd");
+        var checkDate = '';
+		
+		function changeWifi() {
+			
+			currentDate = (new Date()).Format("yyyyMMdd");
 
-
-			setTimeout(function() {
-				try {
-					// 登录
-					document.getElementById('pcPassword').value = '123456';
-					document.getElementById('loginBtn').click();
-				}
-				catch (error) {
-				}
+			if (currentDate != checkDate) {
+				console.log('Different! - currentDate: ' + currentDate + ' | checkDate: ' + checkDate);
 
 				setTimeout(function() {
 					try {
-						// 跳转至修改 WIFI 密码页面
-						parent.frames.bottomLeftFrame.document.getElementById('a9').click();
+						// 登录
+						document.getElementById('pcPassword').value = '123456';
+						document.getElementById('loginBtn').click();
 					}
-					catch (error) {
+					catch (e) {
 					}
 
-                    currentDate = (new Date()).Format("yyyyMMdd");
+					setTimeout(function() {
+						try {
+							// 跳转至修改 WIFI 密码页面
+							parent.frames.bottomLeftFrame.document.getElementById('a9').click();
+						}
+						catch (e) {
+						}
 
-                    if (currentDate != checkDate) {
-                        console.log('different!!!!!!!!');
+						currentDate = (new Date()).Format("yyyyMMdd");
 
+						setTimeout(function() {
+							
+							try {
+								// 修改 WIFI 密码
+								parent.frames.mainFrame.document.getElementById('pskSecret').value = 'Aota' + currentDate;
+								// 保存
+								parent.frames.mainFrame.document.getElementById('Save').click();
 
-                        setTimeout(function() {
-                            // 修改 WIFI 密码
-                            parent.frames.mainFrame.document.getElementById('pskSecret').value = 'Aota' + currentDate;
-                            // 保存
-                            parent.frames.mainFrame.document.getElementById('Save').click();
-                            // alert('修改成功！');
+								setTimeout(function() {
+									// 跳转至重启页面
+									parent.frames.bottomLeftFrame.document.getElementById('a44').click();
 
-                            setTimeout(function() {
-                                // 跳转至重启页面
-                                parent.frames.bottomLeftFrame.document.getElementById('a44').click();
+									setTimeout(function() {
+										// 修改重启提示为 true
+										parent.frames.mainFrame.document.getElementsByTagName("form")[0].onsubmit=true;
+										checkDate = currentDate;
+										// 确认重启
+										parent.frames.mainFrame.document.getElementById('reboot').click();
 
-                                setTimeout(function() {
-                                    // 修改重启提示为 true
-                                    parent.frames.mainFrame.document.getElementsByTagName("form")[0].onsubmit=true;
-                                    checkDate = currentDate;
-                                    // 确认重启
-                                    parent.frames.mainFrame.document.getElementById('reboot').click();
-                                    // parent.frames.bottomLeftFrame.document.getElementById('a9').click();
+										setTimeout(function() {
+											// 跳转到其他页面，以防真的重启而导致刷新页面重新加载JS
+											parent.frames.bottomLeftFrame.document.getElementById('a9').click();
+										}, 1000);
+										
+									}, 1000);
 
-                                    setTimeout(function() {
-                                        // 跳转到其他页面，以防真的重启而导致刷新页面重新加载JS
-                                        parent.frames.bottomLeftFrame.document.getElementById('a9').click();
-                                    }, 2000);
-                                }, 1000);
+								}, 1000);
+							
+							}
+							catch (e) {
+							}
 
-                            }, 1000);
+						}, 1000);
 
-                        }, 1000);
+					}, 1000);
 
-                    } else {
-                        console.log('same!!!!!!!!');
-                    }
+				}, 2000);
+				
+			} else {
+				console.log('Same! - currentDate: ' + currentDate + ' | checkDate: ' + checkDate);
+			}
 
-				}, 1000);
-
-			}, 2000);
 		}
 
 		var myVar;
